4ba0e2a0eade2aecaf63df8b38cce9e2d0ec7bfc
Carlo Hofstetter <carlo_hofstetter@sluz.ch>
2025-09-09 15:45:01 +0200

    task_1_1_abgeschlossen

--- task_1_1.py
+++ task_1_1.py
@@ -1,1 +1,8 @@
-print("Hallo")
+name = input("Wie lautet dein Name?")
+zahl1 = int(input("Nenne eine Zahl:"))
+zahl2 = int(input("Nenne eine zweite Zahl:"))
+summe = zahl1 + zahl2
+
+
+print(name)
+print(summe)
