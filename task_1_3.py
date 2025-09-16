394d51b7649d84664c81a99df3569f70167a79d1
Carlo Hofstetter <carlo_hofstetter@sluz.ch>
2025-09-16 15:09:38 +0200

    task_1_3_abgeschlossen

--- task_1_3.py
+++ task_1_3.py
@@ -3,3 +3,8 @@ name=input("Wie lautet dein Name?")
 print(name.upper())
 print(name.lower())
 print("olraC")
+
+wort1=input("Gebe ein Wort ein: ")
+wort2=input("Gebe ein zweites Wort ein: ")
+
+print(f"{wort1}{wort2}")
